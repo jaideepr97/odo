117fad0996bd8b98b505c2f62e48ef964db83521
Priti Kumari <pkumari@redhat.com>
2020-07-07 21:15:24 +0530

    Update loginlogout_suite_test.go

--- tests/integration/loginlogout/loginlogout_suite_test.go
+++ tests/integration/loginlogout/loginlogout_suite_test.go
@@ -1,10 +1,15 @@
 package integration
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	"github.com/openshift/odo/pkg/util"
+	"github.com/openshift/odo/tests/helper"
 )
 
 func TestLoginlogout(t *testing.T) {
@@ -13,3 +18,22 @@ func TestLoginlogout(t *testing.T) {
 	// Keep CustomReporters commented till https://github.com/onsi/ginkgo/issues/628 is fixed
 	// RunSpecsWithDefaultAndCustomReporters(t, "Loginlogout Suite", []Reporter{reporter.JunitReport(t, "../../../reports")})
 }
+
+
+var tempdir string
+
+var _ = BeforeSuite(func() {
+	originalKubeconfig := os.Getenv("KUBECONFIG")
+	if len(originalKubeconfig) > 0 {
+		tempdir = helper.CreateNewContext()
+		info, err := os.Stat(originalKubeconfig)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = util.CopyFile(originalKubeconfig, filepath.Join(tempdir, "kubeconfig"), info)
+		Expect(err).NotTo(HaveOccurred())
+	}
+})
+
+var _ = AfterSuite(func() {
+	helper.DeleteDir(tempdir)
+})
